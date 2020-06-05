26e35e47cb8fe245b2ecc0e1c31e6e1700423f63
Stefan <stefan@edutoit.net>
2020-06-05 13:02:34 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#index'
-  get 'pages/contact'
-  get 'pages/about' 
+Rails.application.routes.draw do 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
