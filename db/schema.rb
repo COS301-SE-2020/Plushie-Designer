600e1f09fe8c0ffa54d5564fe718e19f4abd7529
HawkEye865 <55985795+HawkEye865@users.noreply.github.com>
2020-06-27 16:02:02 +0200

    Add image collumn to toy table to store screenshot and the images also displays on the index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_125209) do
+ActiveRecord::Schema.define(version: 2020_06_27_133038) do
 
   create_table "ratings", force: :cascade do |t|
     t.text "description"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_06_27_125209) do
     t.float "rleg_posy"
     t.float "rleg_posx"
     t.boolean "shared"
+    t.text "image"
   end
 
   create_table "users", force: :cascade do |t|
