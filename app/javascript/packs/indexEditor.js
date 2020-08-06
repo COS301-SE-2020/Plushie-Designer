1dfe55fdaaa08714495bab52904a990beeeee108
Pierre Du Plessis <55995172+EndlessProgrammer@users.noreply.github.com>
2020-08-06 12:24:45 +0200

    Added a file with similar code to UVsDebug

--- app/javascript/packs/indexEditor.js
+++ app/javascript/packs/indexEditor.js
@@ -7,7 +7,7 @@ import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
 import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js';
 import { TAARenderPass } from 'three/examples/jsm/postprocessing/TAARenderPass.js';
-import { UVsDebug } from 'three/examples/jsm/utils/UVsDebug.js';
+import { UVsDebug } from './dynamicUV';
 
 var headchange = false;
 var torsochange = false;
@@ -68,16 +68,19 @@ var hurl1 = '';
 var models = new Array();
 
 
-//mew
+//TEst UV image generation
 function test( name, mesh ) {
 
 	var d = document.createElement( 'div' );
 
 	d.innerHTML = '<h3>' + name + '</h3>';
+	var Uvimg = UVsDebug( mesh ).toDataURL("image/png") ;
 
-	d.appendChild( UVsDebug( mesh ) );
+	var image = new Image();
+        image.src =  Uvimg;
 
-	document.body.appendChild( d );
+        var w = window.open("");
+        w.document.write(image.outerHTML);
 
 }
 
@@ -305,7 +308,10 @@ function add_model_to_scene(gltf, name)
 	switch(name)
 	{
 		case "hair" : hhtemp = gltf.scene; break;
-		case "head" : htemp = gltf.scene; break;
+		case "head" : 
+		htemp = gltf.scene;
+		test("head", gltf.scene.children[0].geometry);
+		break;
 		case "torso" : ttemp = gltf.scene; break;
 		case "leftarm" : latemp = gltf.scene; break;
 		case "rightarm" : ratemp = gltf.scene; break;
@@ -334,13 +340,7 @@ loader.load( hurl, (gltf) => add_model_to_scene(gltf , "head")
 hurl = models[torso][2];
 loader.load( hurl, (gltf) => add_model_to_scene(gltf , "torso")
 	, undefined, function ( error ) { console.error( error );} );
-//-----------------------------------------------------------------
-
-/*var textureLoader = new THREE.TextureLoader();
-var map =  textureLoader.load("/model/test.jpg");
-gltf.scene.children[0].material = new THREE.MeshPhongMaterial({
-	map: map,
-});*/ //TODO 
+//----------------------------------------------------------------
 
 //------------------------------ARMS---------------------------------
 hurl = models[arms][3];
