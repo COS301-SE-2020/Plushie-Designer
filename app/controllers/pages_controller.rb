3e06bc2a0065c8fa58936e158975a58fa96a95ec
HawkEye865 <55985795+HawkEye865@users.noreply.github.com>
2020-08-24 21:48:30 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,31 @@
 class PagesController < ApplicationController
   def index
-    if current_user
-      redirect_to new_toy_path
+    if !HeadModel.first.nil?
+      if !BodyModel.first.nil?
+        if !LArmModel.first.nil?
+          if !RArmModel.first.nil?
+            if !LLegModel.first.nil?
+              if !RLegModel.first.nil?
+                if current_user
+                  redirect_to new_toy_path
+                end
+              else
+                pages_upload_model_path
+              end
+            else
+              pages_upload_model_path
+            end
+          else
+            pages_upload_model_path
+          end
+        else
+          pages_upload_model_path
+        end
+      else
+        pages_upload_model_path
+      end
+    else
+      pages_upload_model_path
     end
   end
 
